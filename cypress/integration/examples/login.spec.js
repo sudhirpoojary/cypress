0f156b761dff86a17cf40e4350efa00303eff4ff
Sudhir Poojary <spoojary@internal.operative.com>
2020-04-18 17:40:45 +0530

    my first commit

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -9,4 +9,9 @@ describe('Bayzat', function() {
       cy.visit('www.bayzat.com');
         cy.title().should('contain', 'Bayzat 1');
       });
+
+      it('Navigate to Bayzat', function() {
+        cy.visit('www.bayzat.com');
+          cy.title().should('contain', 'Bayzat 1');
+        });
   });
